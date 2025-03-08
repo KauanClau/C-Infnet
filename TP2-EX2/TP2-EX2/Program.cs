2ee9329da52caf5acd2252321c6b9b803009aaf4
Kauan <147768393+KauanClau@users.noreply.github.com>
2025-03-07 22:08:36 -0300

    Add files via upload

---
+++ TP2-EX2/TP2-EX2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+public class CalcularIdade
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Digite sua data de nascimento neste formato (dd/mm/aaaa): ");
+        DateTime dataNascimento = DateTime.Parse(Console.ReadLine());
+
+        DateTime dataAtual = DateTime.Now;
+
+        DateTime proximoAniversario = new DateTime(dataAtual.Year, dataNascimento.Month, dataNascimento.Day);
+
+        if (proximoAniversario < dataAtual)
+        {
+            proximoAniversario = proximoAniversario.AddYears(1);
+        }
+
+        TimeSpan diferenca = proximoAniversario - dataAtual;
+
+        Console.WriteLine($"Faltam {diferenca.Days} dias para o seu próximo aniversário.");
+    } 
+}
