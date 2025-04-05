3688054b70dba6ec5fa4ef648ef63e1580ad123a
Kauan <147768393+KauanClau@users.noreply.github.com>
2025-04-04 23:42:02 -0300

    Add files via upload

---
+++ TP3-C#/ex1/TP3-EX1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+public class Carro
+{
+    // Campos da classe
+    public string cor;
+    public string marca;
+    public string modelo;
+
+    // Método da classe
+    public void Ligar()
+    {
+        Console.WriteLine("O carro está ligado");
+    }
+}
+
+public class Program
+{
+    public static void Main()
+    {
+        // Instanciando um objeto da classe Carro
+        Carro minhaMaquina = new Carro();
+        minhaMaquina.cor = "Cinza claro";
+        minhaMaquina.marca = "Porsche";
+        minhaMaquina.modelo = "911 Carrera 85";
+
+        Console.WriteLine($"Detalhes do carro: \nCor: {minhaMaquina.cor} \nMarca: {minhaMaquina.marca} \nModelo: {minhaMaquina.modelo}");
+
+        minhaMaquina.Ligar();
+    }
+}
